1f7a809a1450c54174b7a3f82cd66756690eba45
mo3az-web <mo3az13579@gmail.com>
2025-05-13 17:09:49 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <BrowserRouter basename='/plumbing-project'>
       <Routes >
         
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/services' element={<Services/>}/>   
