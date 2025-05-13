0ee49249a929fc16034f2499b77d5f32d28b8cec
mo3az-web <mo3az13579@gmail.com>
2025-05-13 18:16:30 +0300

    fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import './Navbar.css'
 import logo_light from "../../assets/logo_light.jpg";
 import logo_dark from "../../assets/logo_dark.jpg";
+import  { useState } from 'react';
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (<>
     <div className='navbar'>
+
         <img src={logo_light} alt="" className='logo'/> مؤسسة عتيوي الرشيد 
-        <ul>
+        <button className="humburger" onClick={toggleMenu}>☰</button>
+        <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+          <a href="" className='exit-button'>X</a>
          <a href="/plumbing-project/">الرئيسيه</a>
             <a href="/plumbing-project/services">خدماتنا</a>
             <a href='/plumbing-project/contact'>اتصل بنا</a>
